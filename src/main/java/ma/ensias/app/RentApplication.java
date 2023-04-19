a16935961314f4e83c86bb7e9ee33ce01c9990ab
oumaacha <mr.anouar.oumaacha@gmail.com>
2023-04-19 16:05:50 +0000

    initial commit

---
+++ src/main/java/ma/ensias/app/RentApplication.java
@@ -0,0 +1,13 @@
+package ma.ensias.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentApplication.class, args);
+	}
+
+}
